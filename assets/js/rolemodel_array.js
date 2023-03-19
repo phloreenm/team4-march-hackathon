466896f6ba78350a5c8b491f4929595f9e2dc34e
emmahewson <emhewson@hotmail.com>
2023-03-19 16:45:03 +0000

    update bio page styling & images, quiz btn styling

--- assets/js/rolemodel_array.js
+++ assets/js/rolemodel_array.js
@@ -2,7 +2,7 @@ let rolemodels = [{
     code: "rmOne",
     score: 0,
     name: "Susan Wojcicki",
-    img: "assets/images/susan_wojcicki.jpg",
+    img: "assets/images/susan_wojcicki.webp",
     alt: "Image of Susan Wojcicki",
     summary: "Susan Diane Wojcicki (born July 5, 1968) was the CEO of YouTube from 2014 to 2023. Wojcicki has worked in the technology industry for over twenty years. She worked as Google's first marketing manager in 1999, and later led the company's online advertising business and original video service. After observing the success of YouTube, she suggested that Google should buy it; the deal was approved for $1.65 billion in 2006. Her net worth was estimated at $765 million in 2022.",
     bioPage: "susan-wojcicki.html"
@@ -11,7 +11,7 @@ let rolemodels = [{
     code: "rmTwo",
     score: 0,
     name: "Annie Easley",
-    img: "assets/images/annie_easley.jpg",
+    img: "assets/images/annie_easley.webp",
     alt: "Image of Annie Easley",
     summary: "Annie Jean Easley (April 23, 1933 - June 25, 2011) was an American computer scientist, mathematician, and rocket scientist. She worked for the National Aeronautics and Space Administration (NASA) and its predecessor, the National Advisory Committee for Aeronautics (NACA). She was a leading member of the team which developed software for the Centaur rocket stage, and was one of the first African-Americans to work at NASA. Easley was posthumously inducted into the Glenn Research Hall of Fame in 2015. On February 1, 2021, a crater on the moon was named after Easley by the IAU.",
     bioPage: "annie-easley.html"
@@ -20,7 +20,7 @@ let rolemodels = [{
     code: "rmThree",
     score: 0,
     name: "Nikki Durkin",
-    img: "assets/images/nikki_durkin.jpg",
+    img: "assets/images/nikki_durkin.webp",
     alt: "Image of Nikki Durkin",
     summary: "Nikki Durkin is an Australian entrepreneur who has made a significant impact in the tech industry through her innovative startups. Durkin's entrepreneurial journey began in 2011 when she founded 99dresses, an online marketplace for trading clothing with virtual currency. Although the startup faced financial difficulties and eventually shut down in 2013, it was a testament to Durkin's ambition and innovation in the industry. Following the closure of 99dresses, Durkin founded Codemakers in 2014, which provided coding education and resources for children.",
     bioPage: "nikki-durkin.html"
@@ -29,7 +29,7 @@ let rolemodels = [{
     code: "rmFour",
     score: 0,
     name: "Hedy Lamarr",
-    img: "assets/images/hedy_lamarr.jpg",
+    img: "assets/images/hedy_lemarr.webp",
     alt: "Image of Hedy Lamarr",
     summary: "Hedy Lamarr (born Hedwig Eva Maria Kiesler; November 9, 1914 - January 19, 2000) was an Austrian-American actress and inventor who pioneered the technology that would one day form the basis for today's WiFi, GPS, and Bluetooth communication systems. A film star during Hollywood's golden age, Lamarr has also been described as one of the greatest movie actresses of all time.",
     bioPage: "hedy-lamarr.html"
@@ -38,7 +38,7 @@ let rolemodels = [{
     code: "rmFive",
     score: 0,
     name: "Mary Wilkes",
-    img: "assets/images/mary_wilkes.jpg",
+    img: "assets/images/mary_wilkes.webp",
     alt: "Image of Mary Wilkes",
     summary: "Mary Allen Wilkes (born September 25, 1937) is a lawyer, former computer programmer and logic designer, known for her work with the LINC computer, now recognized by many as the world's first 'personal computer'. She is also one of the few women computer engineers who made her mark in what was a very male-dominated field in the 1950s and 60s.",
     bioPage: "mary-wilkes.html"
@@ -47,7 +47,7 @@ let rolemodels = [{
     code: "rmSix",
     score: 0,
     name: "Ada Lovelace",
-    img: "assets/images/ada_lovelace.jpg",
+    img: "assets/images/ada_lovelace.webp",
     alt: "Image of Ada Lovelace",
     summary: "Augusta Ada King, Countess of Lovelace (née Byron; 10 December 1815 - 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognise that the machine had applications beyond pure calculation, and to have published the first algorithm intended to be carried out by such a machine. As a result, she is often regarded as the first computer programmer.",
     bioPage: "ada-lovelace.html"
